1039b131b8fba42e55a9fecd822ecc117175e55a
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2020-02-22 10:23:20 +0100

    Add search field to our app

--- src/listContacts.js
+++ src/listContacts.js
@@ -1,33 +1,50 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+
 class ListContacts extends Component {
         static propTypes = {
           contacts: PropTypes.array.isRequired,
           onDeleteContact: PropTypes.func.isRequired,
         }
 
+        state = {
+          query: '',
+        }
+        
         render (){
 
           return (
-              <ul className="contact-list">
-                  {this.props.contacts.map(contact => (
-                      <li key={contact.id } 
-                        className="contact-list-item">
-                        <div className="contact-avatar"
-                          style={
-                            {backgroundImage: `url(${contact.avatarURL})`}  
-                          } >
-                        </div>
-
-                        <div className="contact-details">
-                            <p>{contact.name}</p>
-                            <p>{contact.handle}</p>
-                        </div>
-                        <button className="contact-remove" 
-                        onClick = {() => this.props.onDeleteContact(contact)}></button>
-                      </li>    
-                  ))}
-              </ul>
+             <div className="list-contacts"> 
+               
+               <div className="list-contacts-top">
+                 <input 
+                 className="search-contacts"
+                 type="text"
+                 placeholder="Search Contacts"
+                 value={this.state.query}
+                 />
+               </div> 
+               
+               <ul className="contact-list">
+                   {this.props.contacts.map(contact => (
+                       <li key={contact.id } 
+                         className="contact-list-item">
+                         <div className="contact-avatar"
+                           style={
+                             {backgroundImage: `url(${contact.avatarURL})`}  
+                           } >
+                         </div>
+ 
+                         <div className="contact-details">
+                             <p>{contact.name}</p>
+                             <p>{contact.handle}</p>
+                         </div>
+                         <button className="contact-remove" 
+                         onClick = {() => this.props.onDeleteContact(contact)}></button>
+                       </li>    
+                   ))}
+               </ul>
+             </div>
           )
       }
     
