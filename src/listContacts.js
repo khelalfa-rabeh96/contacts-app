9644081f076f5a76bf00cf5f133c5d88cbd5170d
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2020-02-22 10:13:41 +0100

    Refactor the listCOntacts from functional component to class component syntax

--- src/listContacts.js
+++ src/listContacts.js
@@ -1,29 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function ListContacts (props) {
+class ListContacts extends Component {
     
-        const onDeleteContact = props.onDeleteContact;
-        return (
-            <ul className="contact-list">
-                {props.contacts.map(contact => (
-                    <li key={contact.id } 
-                      className="contact-list-item">
-                      <div className="contact-avatar"
-                        style={
-                          {backgroundImage: `url(${contact.avatarURL})`}  
-                        } >
-                      </div>
+        render (){
 
-                      <div className="contact-details">
-                          <p>{contact.name}</p>
-                          <p>{contact.handle}</p>
-                      </div>
-                      <button className="contact-remove" 
-                      onClick = {() => onDeleteContact(contact)}></button>
-                    </li>    
-                ))}
-            </ul>
-        )
+          return (
+              <ul className="contact-list">
+                  {this.props.contacts.map(contact => (
+                      <li key={contact.id } 
+                        className="contact-list-item">
+                        <div className="contact-avatar"
+                          style={
+                            {backgroundImage: `url(${contact.avatarURL})`}  
+                          } >
+                        </div>
+
+                        <div className="contact-details">
+                            <p>{contact.name}</p>
+                            <p>{contact.handle}</p>
+                        </div>
+                        <button className="contact-remove" 
+                        onClick = {() => this.props.onDeleteContact(contact)}></button>
+                      </li>    
+                  ))}
+              </ul>
+          )
+      }
     
 }
 export default ListContacts
